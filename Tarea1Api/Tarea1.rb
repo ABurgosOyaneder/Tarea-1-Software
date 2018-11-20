d035158ec77932cbad761c5d48689f8f9a5a1ff9
Alexis Burgos Oyaneder <38987071+ABurgosOyaneder@users.noreply.github.com>
2018-11-20 12:00:12 -0300

    Update Tarea1.rb

--- Tarea1Api/Tarea1.rb
+++ Tarea1Api/Tarea1.rb
@@ -1,12 +1,18 @@
 
-	require 'httparty'
-	require 'openssl'
+require 'httparty'
+require 'openssl'
 
-	URL = 'https://sepa.utem.cl/rest/api/v1'
-	AUTH = {username: '2E5Vw7WUCm', password: '0754e0cbf6df85c40be0715e643c9f1c'}
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 
-	def anio
+URL='https://sepa.utem.cl/rest/api/v1'
+AUTH = {username: '2E5Vw7WUCm', password: '0754e0cbf6df85c40be0715e643c9f1c'}
+
+#Anio Docentes
+#
+#
+#
+def anio
   docente = '/academia/docentes'
   respuesta = HTTParty.get(URL.encode+docente, basic_auth: AUTH)
 
@@ -45,34 +51,38 @@ def asignaturas1
   puts lista
 end
 
-	def punto_tres
-	    docentes= "/academia/docentes"
-	    respuesta = HTTParty.get(URI.encode(URL+docentes), basic_auth: AUTH)
-	    anio = []
-	    respuesta.each do |docentes|
-	    	anio << f['anio']
-	    	if f['anio']<=1980 
-	    		promedio_uno = promedio_uno + f['nota'].to_f
-	    		ct_uno = ct_uno + 1
-	    	end
-	    end
-	    filtro = anio.uniq 
-	    
+def anio2015
+  #asignatura2015 = '/docencia/asignaturas/'{codigo}'/cursos'
+  respuesta = HTTParty.get(URL.encode+asignatura2015, basic_auth: AUTH)
+  lista = []
+  lista2 = []
+  respuesta.each do |data|
+    lista2 = data['anio']
+    if lista2 == 2015
+    lista << data['asignatura']
+    lista << data['anio']
+    end
+    File.open('ABurgosOyaneder',"w") do |f|
+      f.puts lista
+    end
+  end
+  puts lista
+end
 
-	end
 
 	def menu
 		puts "Ingrese seleccion : "
 		puts " 1. Mostrar todas las asignaturas con codigo INF"
 		puts " 2. Mostrar el total de las asignaturas con codigo INF impartidas el 2015"
 		puts " 3. Crear nómina de todos los docentes nacidos antes del 1980. "
+        puts "4.- Salir\n"
 		print "Seleccion: "
 		seleccion = gets.chomp.to_i
 		 case seleccion
-			when 1 then punto_uno
-			when 2 then punto_dos
-			when 3 then punto_tres
-			when 4 then punto_cuatro
+			when 1 then asignaturas1
+			when 2 then anio2015
+			when 3 then anio
+			when 4 print 'adios'
 			else 	
 		end
 	end
